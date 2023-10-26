32bae3809c941de5c2c0e545bef357f559af82a9
Soham <sohamborkar14@gmail.com>
2023-10-26 23:25:02 +0530

    initial commit

---
+++ src/main/java/com/mycompany/feedback_mng_sys/Feedback_mng_sys.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.feedback_mng_sys;
+
+/**
+ *
+ * @author soham
+ */
+public class Feedback_mng_sys {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Freakin World!");
+    }
+}
