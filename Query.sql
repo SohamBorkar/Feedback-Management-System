3e0906de69a54445f11304f1cb1244ff643fe76c
Amey Pathe <ameypte@gmail.com>
2023-10-28 00:08:35 +0530

    added query file

---
+++ Query.sql
@@ -0,0 +1,16 @@
+TRUNCATE TABLE multiuserlogin;
+
+ALTER TABLE multiuserlogin
+ADD PRIMARY KEY (id);
+
+INSERT INTO multiuserlogin VALUES(100,'amey123','Student');
+INSERT INTO multiuserlogin VALUES(101,'soham123','Student');
+INSERT INTO multiuserlogin VALUES(102,'pratik123','Student');
+
+INSERT INTO multiuserlogin VALUES(1,'fac1','Faculty');
+INSERT INTO multiuserlogin VALUES(2,'fac2','Faculty');
+INSERT INTO multiuserlogin VALUES(3,'fac3','Faculty');
+INSERT INTO multiuserlogin VALUES(4,'fac4','Faculty');
+
+INSERT INTO multiuserlogin VALUES(1001,'admin1','Admin');
+INSERT INTO multiuserlogin VALUES(1002,'admin2','Admin');
